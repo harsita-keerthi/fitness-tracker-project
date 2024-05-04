e4d068342f0d51d3b373f0ad2e8a0bbc063396c3
harsita-keerthi <131437586+harsita-keerthi@users.noreply.github.com>
2024-05-04 12:51:13 -0700

    update User class

--- src/User.java
+++ src/User.java
@@ -7,12 +7,58 @@ public class User {
     private List workouts;
 
     public User (String name, double age, double height, double weight){
-        // ... set attributes
+        this.name = name;
+        this.age = age;
+        this.weightLbs = weight;
+        this.heightIn = height;
     }
 
-    //getters and setters
+    // getters and setters
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public double getAge(){
+        return age;
+    }
+
+    public void setAge(int age){
+        this.age = age;
+    }
+
+    public double getWeight(){
+        return weightLbs;
+    }
+
+    public void setWeight(double weight){
+        this.weightLbs = weight;
+    }
+
+    public double getHeight(){
+        return heightIn;
+    }
+
+    public void setHeight(double height){
+        this.heightIn = height;
+    }
     // updating personal info
+    public void updatePersonalInfo(int age, double weight, double height){
+        this.age = age;
+        this.weightLbs = weight;
+        this.heightIn = height;
+    }
     // method for accessing user's workouts
+    public void addWorkout(Workout workout){
+        workouts.add(workout);
+    }
+
+    public List<Workout> getWorkouts(){
+        return workouts;
+    }
 
 
 }
