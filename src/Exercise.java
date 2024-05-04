04c52c601639c98a6876f174d34f9b78c65857df
harsita-keerthi <131437586+harsita-keerthi@users.noreply.github.com>
2024-05-04 12:59:35 -0700

    update Exercise class

--- src/Exercise.java
+++ src/Exercise.java
@@ -5,9 +5,44 @@ abstract class Exercise {
     private String description;
 
     public Exercise(String activityType, String name, int caloriesBurned, String description){
-        //... initialize attributes
+        this.activityType = activityType;
+        this.name = name;
+        this.caloriesBurned = caloriesBurned;
+        this.description = description;
     }
 
     //getters and setters
+    public String getActivityType(){
+        return activityType;
+    }
+
+    public void setActivityType(String activityType){
+        this.activityType = activityType;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public int getCaloriesBurned(){
+        return caloriesBurned;
+    }
+
+    public void setCaloriesBurned(int caloriesBurned){
+        this.caloriesBurned = caloriesBurned;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public void setDescription(String description){
+        this.description = description;
+    }
     //abstract methods, extend classes
+    public abstract void displayDetails();
 }
