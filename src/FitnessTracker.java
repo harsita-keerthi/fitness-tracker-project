2e35e88b26987cf7d67e5c0eefd8ba6ee86a0dbc
amaradelprato <145729120+amaradelprato@users.noreply.github.com>
2024-05-07 02:17:37 -0700

    continued fleshing out menu options

--- src/FitnessTracker.java
+++ src/FitnessTracker.java
@@ -8,14 +8,117 @@ import java.util.*;
 public class FitnessTracker {
 	public static void main(String[] args) {
 		/* WELCOME MESSAGE */
-		System.out.println("* Welcome to Fitness Tracker! *");
+		System.out.println("* WELCOME TO FITNESS TRACKER *");
 		System.out.println();
 		
 		User user = createUser();
 		
 		/* MENU DISPLAY */
 		int choice = displayMenu();
-		System.out.println("You chose option " + choice);
+		while (choice != 0) {
+			switch(choice) {
+				case 1:
+					System.out.println("Creating new workout...");
+					System.out.println();
+					
+					Scanner scnr = new Scanner(System.in);
+					System.out.print("Date completed: ");
+					String date = scnr.nextLine();
+					
+					System.out.print("Time completed: ");
+					String time = scnr.nextLine();
+					
+					System.out.print("Total duration: ");
+					int duration = scnr.nextInt();
+					System.out.println();
+					
+					Workout workout = new Workout(date, time, duration);
+					user.addWorkout(workout);
+					scnr.nextLine();
+					
+					System.out.print("Would you like to add an exercise? (y/n): ");
+					String addAnother = scnr.nextLine();
+					int type = 0;
+					while (addAnother.equals("y")) {
+						System.out.println("Choose one of the following exercise types:");
+						System.out.println("1 - weightlifting");
+						System.out.println("2 - stretching");
+						System.out.println("3 - cardio");
+						System.out.println("4 - mind and body");
+						System.out.print("Exercise type: ");
+						type = scnr.nextInt();
+						scnr.nextLine();
+						System.out.println();
+						
+						System.out.print("Exercise name: ");
+						String name = scnr.nextLine();
+						System.out.print("Exercise description: ");
+						String description = scnr.nextLine();
+						System.out.print("Calories burned: ");
+						int cals = scnr.nextInt();
+						scnr.nextLine();
+						
+						Exercise e = null;
+						switch(type) {
+							case 1: 
+								e = new Weightlifting("weightlifting", name, cals, description);
+								break;
+							case 2:
+								e = new Stretching("stretching", name, cals, description);
+								break;
+							case 3:
+								e = new Cardio("cardio", name, cals, description);
+								break;
+							case 4:
+								e = new MindBody("mind and body", name, cals, description);
+								break;
+						}
+						
+						workout.addExercise(e);
+						System.out.println();
+						System.out.print("Would you like to add an exercise? (y/n): ");
+						addAnother = scnr.nextLine();
+					}
+					System.out.println();
+					break;
+				case 2:
+					System.out.println("Displaying workouts...");
+					System.out.println();
+					
+					for (Workout w : user.getWorkouts()) {
+						w.displaySummary();
+					}
+					
+					break;
+				case 3:
+					System.out.println("Displaying workouts..."); // FIXME: left off here
+					System.out.println();
+					break;
+				case 4:
+					System.out.println("Creating fitness goal...");
+					System.out.println();
+					break;
+				case 5:
+					System.out.println("Displaying fitness goals...");
+					System.out.println();
+					break;
+				case 6:
+					System.out.println("Getting goal advice...");
+					System.out.println();
+					break;
+				case 7:
+					System.out.println("Displaying account info...");
+					System.out.println();
+					break;
+				case 8:
+					System.out.println("Displaying account info...");
+					System.out.println();
+					break;
+			}
+			choice = displayMenu();
+		}
+		
+		System.out.println("Application closing...");
 	}
 	
 	public static User createUser() {
@@ -23,7 +126,6 @@ public class FitnessTracker {
 		Scanner scnr = new Scanner(System.in);
 		System.out.print("To begin, please enter your name: ");
 		String name = scnr.nextLine();
-		System.out.println();
 		
 		// ask age
 		boolean ageValid = false; 
@@ -39,7 +141,6 @@ public class FitnessTracker {
                 scnr.nextLine(); 
             }
         }
-		System.out.println();
 		
 		// ask height
 		boolean heightValid = false; 
@@ -55,7 +156,6 @@ public class FitnessTracker {
                 scnr.nextLine(); 
             }
         }
-		System.out.println();
 		
 		// ask weight
 		boolean weightValid = false; 
@@ -85,12 +185,21 @@ public class FitnessTracker {
 		
 		System.out.println("1 - Create a workout");
 		System.out.println("2 - Display workouts");
+		System.out.println("3 - Edit a workout");
+		System.out.println("4 - Set fitness goal");
+		System.out.println("5 - Display fitness goals");
+		System.out.println("6 - Get goal advice");
+		System.out.println("7 - Display account info");
+		System.out.println("8 - Edit account info");
 		System.out.println("0 - Quit application");
 		
 		System.out.println();
 		Scanner scnr = new Scanner(System.in);
 		System.out.print("Enter key: ");
 		int key = scnr.nextInt();
+		System.out.println();
+		System.out.println("---------------------------------------------------");
+		System.out.println();
 		return key;
 	}
 }
